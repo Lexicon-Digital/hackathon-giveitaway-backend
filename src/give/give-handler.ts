6478598cb6a2072eda7d0435cf2f6501fce8fc1f
Dan Nichols <dan@lexicondigital.com.au>
2021-12-21 10:57:15 +1000

    Adding give endpoint

---
+++ src/give/give-handler.ts
@@ -0,0 +1,58 @@
+import { APIGatewayProxyHandler } from "aws-lambda";
+import {
+  DynamoDBClient,
+  PutItemCommand,
+  PutItemCommandInput,
+} from "@aws-sdk/client-dynamodb";
+import { marshall } from "@aws-sdk/util-dynamodb";
+import { v4 as uuid } from "uuid";
+
+interface Item {
+  id: string;
+  title: string;
+  description: string;
+  imageUrl: string;
+  listedTime: string;
+  location: Location;
+}
+
+interface Location {
+  lat: number;
+  lng: number;
+}
+
+export const handler: APIGatewayProxyHandler = async (event, context) => {
+  var postData: Item = JSON.parse(event.body || "");
+  postData.id = uuid();
+
+  const client = new DynamoDBClient({ region: "ap-southeast-2" });
+
+  const params: PutItemCommandInput = {
+    TableName: "giveitaway-items",
+    Item: marshall(postData),
+  };
+
+  console.log("Adding a new item...");
+
+  var response = {
+    statusCode: 201,
+    body: "",
+  };
+
+  const run = async function () {
+    try {
+      const results = await client.send(new PutItemCommand(params));
+      console.log(results);
+    } catch (err) {
+      console.error(err);
+      response = {
+        statusCode: 500,
+        body: "",
+      };
+    }
+  };
+
+  await run();
+
+  return response;
+};
